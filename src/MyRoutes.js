27b4a2349f3132255150d6eff6bf3232a567858e
pangil5634 <oksk6681@naver.com>
2024-02-07 16:41:44 +0900

    add Graph 테스트 페이지

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -5,6 +5,7 @@ import Page2 from "./Pages/Page2";
 import Page3 from "./Pages/Page3";
 import Page4 from "./Pages/Page4";
 import Header from "./Header";
+import GraphTest from "./Pages/Test/GraphTest";
 
 function MyRoutes() {
     return (
@@ -14,7 +15,8 @@ function MyRoutes() {
                 <Route path="/page1" element={<Page1 />}/>
                 <Route path="/page2" element={<Page2/>}/>
                 <Route path="/page3" element={<Page3/>}/>
-                <Route path="/page4" element={<Page4/>}/>
+                <Route path="/page4" element={<Page4 />} />
+                <Route path="/test1" element={<GraphTest/>}/>
             </Route>
         </Routes>
     );
