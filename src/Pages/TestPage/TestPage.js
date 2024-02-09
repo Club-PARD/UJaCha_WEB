c846b856c97ae4317eb725504ba91f32d7d46f34
minzziPark <minzzi0068@gmail.com>
2024-02-09 16:51:28 +0900

    style : 질문 컨테이너 css 완료

--- src/Pages/TestPage/TestPage.js
+++ src/Pages/TestPage/TestPage.js
@@ -8,6 +8,7 @@ import TestPageButton from "./Components/TestPageButton";
 function TestPage() {
   return (
     <TestPageContainer>
+      <QuestionContainer></QuestionContainer>
       {/* <TestPageHeader /> */}
       {/* <TestPageQuestion /> */}
       {/* <TestPageAnswer /> */}
@@ -20,5 +21,14 @@ const TestPageContainer = styled(Container)`
   height: 100vh;
   width: 100vw;
   background-color: black;
+`;
+
+const QuestionContainer = styled.div`
+  width: 100vw;
+  height: 88vh;
+  border-radius: 24px 24px 0px 0px;
+  background-color: ${({ theme }) => theme.colors.purple_100};
+  position: absolute;
+  bottom: 0px;
 `;
 export default TestPage;
