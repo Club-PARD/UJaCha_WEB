8c104471cc43d6cdec9e67fe77a863166b13d77f
minzziPark <minzzi0068@gmail.com>
2024-02-09 17:29:44 +0900

    style : 버튼 크기 조절

--- src/Pages/TestPage/Components/TestPageButton.js
+++ src/Pages/TestPage/Components/TestPageButton.js
@@ -1,66 +1,63 @@
 import styled from "styled-components";
-import {Container} from "../../../Layout/Layout";
-import {useEffect, useState} from "react";
+import { Container } from "../../../Layout/Layout";
+import { useEffect, useState } from "react";
 
 function TestPageButton() {
+  const [questionCount, setQuestionCount] = useState(1);
 
-    const [questionCount, setQuestionCount] = useState(1);
+  useEffect(() => {
+    console.log("questionCount", questionCount);
+  }, [questionCount]);
 
-    useEffect(() => {
-        console.log("questionCount", questionCount);
-    }, [questionCount]);
-
-    const handleDecreaseQuestionCount = () => {
-        if (questionCount === 1) {
-            alert("첫 페이지입니다.");
-        } else {
-            setQuestionCount(questionCount - 1);
-        }
+  const handleDecreaseQuestionCount = () => {
+    if (questionCount === 1) {
+      alert("첫 페이지입니다.");
+    } else {
+      setQuestionCount(questionCount - 1);
     }
+  };
 
-    const handleIncreaseQuestionCount = () => {
-        if (questionCount === 10) {
-            alert("마지막 페이지입니다.");
-        } else {
-            setQuestionCount(questionCount + 1);
-        }
+  const handleIncreaseQuestionCount = () => {
+    if (questionCount === 10) {
+      alert("마지막 페이지입니다.");
+    } else {
+      setQuestionCount(questionCount + 1);
     }
-    return (
-        <TestPageButtonContainer>
-            <WrapperButton>
-                <Button onClick={handleDecreaseQuestionCount}>이전</Button>
-                <Button onClick={handleIncreaseQuestionCount}>다음</Button>
-            </WrapperButton>
-        </TestPageButtonContainer>
-    );
+  };
+  return (
+    <TestPageButtonContainer>
+      <WrapperButton>
+        <Button onClick={handleDecreaseQuestionCount}>이전</Button>
+        <Button onClick={handleIncreaseQuestionCount}>다음</Button>
+      </WrapperButton>
+    </TestPageButtonContainer>
+  );
 }
 
 const TestPageButtonContainer = styled(Container)`
-    height : 200px;
+  height: 200px;
 
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
-const WrapperButton = styled.div `
-    width: 90%;
+const WrapperButton = styled.div`
+  width: 86vw;
+  display: flex;
+  justify-content: space-between;
+`;
 
-    display: flex;
-    justify-content: space-between;
-`
+const Button = styled.button`
+  width: 42vw;
+  height: 6.29vh;
+  font-size: 15px;
+  background: none;
+  border: 1px solid black;
+  border-radius: 16px;
 
-const Button = styled.button `
-    width : 167px;
-    height : 51px;
-
-    font-size: 15px;
-
-    background : none;
-    border : 1px solid black;
-
-    &:hover{
-        opacity: 50%;
-        background-color: #00000050;
-    }
+  &:hover {
+    opacity: 50%;
+    background-color: #00000050;
+  }
 `;
 export default TestPageButton;
