853af46234078d0ec13db17bf8b04cea663122a1
minzziPark <minzzi0068@gmail.com>
2024-02-14 01:59:48 +0900

    feat : 데이터 전송 오류 해결

--- src/Pages/TestPage/Components/TestLoading.js
+++ src/Pages/TestPage/Components/TestLoading.js
@@ -1,14 +1,29 @@
 import { Img } from "../../../Layout/Layout";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import { postFirstData } from "../../../Api/test";
+import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
+import { formState, resultState } from "../../../Atoms";
+import { useNavigate } from "react-router-dom";
 
 function TestLoading() {
+  const movePage = useNavigate();
+  const form = useRecoilValue(formState);
+  const [result, setResult] = useRecoilState(resultState);
   const [showLoding, setShowLoding] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLoding(true);
     }, 100);
+
+    const timeout = setTimeout(async () => {
+      const response = await postFirstData(form);
+      setResult(response.data);
+      console.log(response.data);
+      console.log(result);
+      movePage("/result");
+    }, 3000);
 
     return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머 해제
   }, []); // 초기 렌더링 시 한 번만 실행
