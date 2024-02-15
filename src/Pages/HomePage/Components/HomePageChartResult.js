31df0003aed8a5f13cb5d59fba190656cc97aa21
pangil5634 <oksk6681@naver.com>
2024-02-16 01:33:34 +0900

    edit : code 정리

--- src/Pages/HomePage/Components/HomePageChartResult.js
+++ src/Pages/HomePage/Components/HomePageChartResult.js
@@ -1,55 +1,77 @@
 import styled from "styled-components";
-import { P } from "../../../Layout/Layout";
-import { constSelector } from "recoil";
-import { MiniSquare, sortByMaxValue } from "./tempChartData";
-import { theme } from "../../../Styles/theme";
+import {P} from "../../../Layout/Layout";
+import {MiniSquare, sortByMaxValue} from "./tempChartData";
+import {theme} from "../../../Styles/theme";
+
+// [ 바로가기 ]
+// Container : HomePageChartResult
+// Wrapper : Content (잦은 증상 순위)
+// handler 증상에 따라 배경색을 반환하는 함수
+// handler : 각 증상에 따라 symbol을 반환하는 함수
+// Component : 증상 별 사각형 색상 아이콘
+// Component : styled Hr 태그
 
 const HomePageChartResult = (e) => {
-    const data = ["가장 잦습니다.", "두 번째로 잦습니다.", "세 번째로 잦습니다."];
-    const temp = [{}]
-    const dataTemp = sortByMaxValue(e.lastedData);
-    const topThreeSymptoms = Object.entries(dataTemp)
+
+    // 변수 선언
+    const resultEndString = ["가장 잦습니다.", "두 번째로 잦습니다.", "세 번째로 잦습니다."];
+    const sortedLastedData = sortByMaxValue(e.lastedData);
+    const topThreeLastedData = Object
+        .entries(sortedLastedData)
         .filter(([key]) => key !== 'total') // total 제외
-        .sort(([, valueA], [, valueB]) => valueB - valueA) // 값에 따라 내림차순 정렬
+        .sort(([
+            , valueA
+        ], [, valueB]) => valueB - valueA) // 값에 따라 내림차순 정렬
         .slice(0, 3); // 상위 3개 항목 선택
 
-    console.log(topThreeSymptoms);
-    
+    // console.log(topThreeLastedData);  가장 높은 수치의 세 가지 '증상' 출력
+
     return (
         <HomePageChartResultContainer>
+            {/* 가장 잦은 증상*/}
             <P fontSize="16px" fontWeight="500">가장 잦은 증상</P>
+
+            {/* 잦은 증상 순위*/}
             <WrapperContent>
-                {topThreeSymptoms.map(([symptom, value], index) => (
-                    <P fontSize="16px" fontWeight="400" key={index}>
-                        <StyledMiniSquare backgroundColor={getBackgroundColor(symptom)} />
-                        {getSymbol(symptom)} 증상이 {value}%로 {data[index]} 
-                    </P>
-                ))}
+                {
+                    topThreeLastedData.map(([symptom, value], index) => (
+                        <P fontSize="14px" fontWeight="400" key={index}>
+                            <StyledMiniSquare backgroundColor={getBackgroundColor(symptom)} />
+                            {getSymbol(symptom)} 증상이 평균 {value}%로 {resultEndString[index]}
+                            {/* ex) 감정 변화 증상이 평균 65%로 가장 잦습니다. */}
+                        </P>
+                    ))
+                }
             </WrapperContent>
-            <MyHr/>
-            <P fontSize="16px" fontWeight="400"><MiniSquare backgroundColor = "black"/>조현병 의심 정도는 {dataTemp.total}%입니다.</P>
+
+            <MyHr />
+            
+            <P fontSize="14px" fontWeight="400"><MiniSquare backgroundColor="black"/>조현병 의심 정도는 {sortedLastedData.total}%입니다.</P>
         </HomePageChartResultContainer>
     );
 }
 
-const HomePageChartResultContainer = styled.div`
+// Container : HomePageChartResult
+const HomePageChartResultContainer = styled.div `
     width: 100%;
     height : 100%;
+
     /* background-color: yellow; */
 `
-const WrapperContent = styled.div`
-
-    margin-top: 20px;
+// Wrapper : Content (잦은 증상 순위)
+const WrapperContent = styled.div `
     width: 100%;
     height :80px;
-
+    
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+
+    margin-top: 20px;
     /* background-color: red; */
 `
 
-// 증상에 따라 배경색을 반환하는 함수
+// handler 증상에 따라 배경색을 반환하는 함수
 const getBackgroundColor = (symptom) => {
     switch (symptom) {
         case 'hallucination':
@@ -65,7 +87,7 @@ const getBackgroundColor = (symptom) => {
     }
 }
 
-// 각 증상에 따라 symbol을 반환하는 함수
+// handler : 각 증상에 따라 symbol을 반환하는 함수
 const getSymbol = (symptom) => {
     switch (symptom) {
         case 'hallucination':
@@ -81,13 +103,17 @@ const getSymbol = (symptom) => {
     }
 }
 
+// Component : 증상 별 사각형 색상 아이콘
 const StyledMiniSquare = styled(MiniSquare)`
-    background-color: ${({ backgroundColor }) => backgroundColor};
+    background-color: ${props => props.backgroundColor};
 `;
 
-const MyHr = styled.hr`
+// Component : styled Hr 태그
+const MyHr = styled.hr `
     margin : 15px 0px;
-    border : 1px solid gray;
+    
+    border : none;
+    border-bottom : 1px solid gray;
 `
 
 export default HomePageChartResult;
