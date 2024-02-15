eec5062367012ac9d85799838f68f9780996b5c6
minzziPark <minzzi0068@gmail.com>
2024-02-16 01:06:45 +0900

    feat : 인트로 페이지에 delete 버튼 임시 설치

--- src/Pages/IntroPage/IntroPage.js
+++ src/Pages/IntroPage/IntroPage.js
@@ -1,44 +1,53 @@
 import styled from "styled-components";
-import {Container} from "../../Layout/Layout";
+import { Container } from "../../Layout/Layout";
 import IntroPageContent from "./Components/IntroPageContent";
 import IntroPageMain from "./Components/IntroPageMain";
 import IntroPageSub from "./Components/IntroPageSub";
 import IntroPageBottom from "./Components/IntroPageBottom";
 import IntroPageHeader from "./Components/IntroPageHeader";
+import { deleteUser } from "../../Api/test";
 
 function IntroPage() {
-    return (
-        <BlackContainer>
-            <IntroPageContainer>
-                <IntroPageHeader/>
-                <IntroPageMain/>
-                <IntroPageContent/>
-                <IntroPageSub/>
-                <IntroPageBottom/>
-            </IntroPageContainer>
-        </BlackContainer>
-    );
+  const handleClick = () => {
+    const response = deleteUser();
+    console.log(response);
+  };
+
+  return (
+    <BlackContainer>
+      <IntroPageContainer>
+        <button onClick={handleClick} style={{ color: "white" }}>
+          유저 삭제
+        </button>
+        <IntroPageHeader />
+        <IntroPageMain />
+        <IntroPageContent />
+        <IntroPageSub />
+        <IntroPageBottom />
+      </IntroPageContainer>
+    </BlackContainer>
+  );
 }
 
 const IntroPageContainer = styled(Container)`
-    height : auto;
+  height: auto;
 
-    /* background-color: red; */
+  /* background-color: red; */
 
-    padding : 0px 8px;
-    box-sizing: border-box;
-`
+  padding: 0px 8px;
+  box-sizing: border-box;
+`;
 
 export const BlackContainer = styled.div`
-    width: 100vw;
-    height: ${props => props.height};
+  width: 100vw;
+  height: ${(props) => props.height};
 
-    background-color: black;
-    
-    display: flex;
-    justify-content: center;
-    align-items: ${props => props.alignItems};
+  background-color: black;
 
-    flex-direction: ${props => props.flexDirection};
-`
+  display: flex;
+  justify-content: center;
+  align-items: ${(props) => props.alignItems};
+
+  flex-direction: ${(props) => props.flexDirection};
+`;
 export default IntroPage;
