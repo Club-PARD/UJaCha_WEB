60337c034403af5bb74054d4fc1d2c3073829074
pangil5634 <oksk6681@naver.com>
2024-02-16 02:33:53 +0900

    edit : remove page1~3 file

--- src/Pages/Page1.js
+++
@@ -1,9 +0,0 @@
-function Page1() {
-    return (
-        <div>
-            <h1>서브 페이지1</h1>
-        </div>
-    );
-}
-
-export default Page1;
--- src/Pages/Page2.js
+++
@@ -1,9 +0,0 @@
-function Page2() {
-    return (
-        <div>
-            <h1>서브 페이지2</h1>
-        </div>
-    );
-}
-
-export default Page2;
