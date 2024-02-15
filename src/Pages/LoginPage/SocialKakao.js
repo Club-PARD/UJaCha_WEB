8ad8df8ef1ab0440c31ef19d1e28ec4639646baf
pangil5634 <oksk6681@naver.com>
2024-02-16 03:06:47 +0900

    edit : code 정리

--- src/Pages/LoginPage/SocialKakao.js
+++ src/Pages/LoginPage/SocialKakao.js
@@ -5,56 +5,77 @@ import axios from "axios";
 import { Button } from "../IntroPage/Components/IntroPageMain";
 import { useRecoilState } from "recoil";
 import { userInfo } from "../../Atoms";
-const SocialKakao = ({ page }) => {
-  const [userInfoData, setUserInfoData] = useRecoilState(userInfo); // 유저 정보 저장
+import styled from "styled-components";
+import { theme } from "../../Styles/theme";
 
+const SocialKakao = ({ page }) => {
+  // Variables
+  const [userInfoData, setUserInfoData] = useRecoilState(userInfo); // 유저 정보 저장
   const kakaoClientId = process.env.REACT_APP_REST_API_KEY;
   const navigate = useNavigate();
 
+  // handler
   const KakaoOnSuccess = async (data) => {
-    console.log(data);
-    await window.localStorage.clear();
-    await sessionStorage.setItem("userKakaoId", data.profile.id);
-    await sessionStorage.setItem(
-      "kakaoAccessToken",
-      data.response.access_token
-    );
+
+    // sessionStorage 초기화
+    window.localStorage.clear();
+    window.sessionStorage.clear();
+
+    // sessionStorage 저장
+    sessionStorage.setItem("userKakaoId", data.profile.id);
+    sessionStorage.setItem("kakaoAccessToken", data.response.access_token);
+    
+    // kakao login을 위한 request 정보
     const loginData = {
       // kakaoAccessToken: data.response.access_token
       email: data.profile.kakao_account.email,
       uid: data.profile.id,
     };
-    console.log("loginData", loginData);
 
     // 로그인 성공 시 SignupPage 페이지로 이동
     try {
-      console.log("test", process.env.REACT_APP_URL + "/api/member/login");
+
+      // 로그인 시도
       const response = await axios.post(
         process.env.REACT_APP_URL + "/api/member/login",
         loginData
-        // {withCredentials: true}  요청에 인증 정보를 포함하여 보냄
       );
-      console.log("유자차 response", response);
+
+      // 로그인 결과 출력
+      console.log("kako login response", response);
+
+      // 처음 접속 여부 판단
       if (response.data.first === true) {
-        console.log("Server Response:", response.data);
+        
+        // uid를 recoil로 context 저장하기 위한 변수 선언
         const tempUserInfoData = {
           nickname: "",
           reliableName: "",
           childAge: null,
           uid: data.profile.id,
         };
+
+        // 유저 정보context 저장
         setUserInfoData(tempUserInfoData);
+
+        // 회원가입 페이지로 이동 
         navigate("/register");
       } else {
-        console.log(response.data.token);
+        // 처음 접속이 아닌 경우
+
+        // sessionStorage에 jwtToken 저장
         sessionStorage.setItem("jwtToken", response.data.token);
+
+        // 홈페이지로 이동
         navigate("/home");
       }
+
     } catch (error) {
       console.error("Error sending login data:", error);
     }
   };
 
+  // 로그인 실패했을 경우
   const kakaoOnFailure = (error) => {
     console.log(error);
   };
@@ -68,24 +89,22 @@ const SocialKakao = ({ page }) => {
         onFail={kakaoOnFailure}
         render={({ onClick }) =>
           page === "result" ? (
-            <Button
-              onClick={onClick}
-              backgroundcolor="black"
-              color="white"
-              margintop="57px"
-            >
-              카카오 로그인
-            </Button>
+            <KakaoLoginButton onClick={onClick} backgroundcolor="black" color="white" margintop="57px" >카카오 로그인</KakaoLoginButton>
           ) : (
-            <Button onClick={onClick} backgroundcolor="transparent">
-              {/* <img src={KakaoBtn} alt="KakaoBtn"/> */}
-              카카오 로그인
-            </Button>
+            <KakaoLoginButton onClick={onClick} backgroundcolor="yellow"> 카카오 로그인</KakaoLoginButton>
           )
         }
       />{" "}
     </div>
   );
 };
+
+// 카카오 로그인 버튼
+const KakaoLoginButton = styled(Button)`
+  background-color: transparent;
+  &:hover{
+    background-color: ${theme.colors.pruple_bold};
+  }
+`
 
 export default SocialKakao;
