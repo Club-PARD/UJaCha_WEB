ec4c74250a942003a4af3b28413d3166272ef848
KimRPG <hunsung9909@gmail.com>
2024-02-17 11:46:43 +0900

    fix : 포트 바꾸기

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (_, res) => {
 	res.sendFile(path.join(__dirname, '/build/index.html'))
 })
 
-const PORT = 80;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`); // 정상동작 확인용 주석
